d322155d8c8a080c348c602c4d7773e01c8c601e
Marcos Francisco Ribeiro <136476694+marcosfranrib@users.noreply.github.com>
2023-09-16 16:47:14 -0300

    .

--- joken.js
+++ joken.js
@@ -8,18 +8,33 @@ let contador = 1;
 
 pedra.addEventListener('click', ()=>{
     let numeroSorteado = Math.floor(3* Math.random())+1;
-    console.log('NUMERO: ',numeroSorteado);
+    
+    pedra.style.background = '#F0F0F0'
+    papel.style.background = '#F0F0F0'
+    tesoura.style.background = '#F0F0F0'
+    pedra.style.color = 'white'
+    papel.style.color = 'white'
+    tesoura.style.color = 'white'
 
     if (numeroSorteado == 1) {
+        pedra.style.background = 'green'
+        tesoura.style.background = 'red'
+        papel.style.color = 'black'
+
         resultado.innerHTML = 'TESOURA, VOCÊ VENCEU';    
         placar.innerHTML = `PLACAR: ${contador++}`
 
     }else if (numeroSorteado == 2) {
         resultado.innerHTML = 'PAPEL, VOCÊ PERDEU'
-    
+        pedra.style.background = 'red'
+        papel.style.background = 'green'
+        tesoura.style.color = 'black'
+
     }else if (numeroSorteado == 3) {
         resultado.innerHTML = 'PEDRA, EMPATOU'
-    
+        pedra.style.background = 'black'
+        papel.style.color = 'black'
+        tesoura.style.color = 'black'
     }
 
 })
